8710fbd1ee4631115f61d42a7bc1ab12f0490c97
stanJ <cjiao@3ti.us>
2017-08-29 18:06:54 +0800

    第一个test页面

---
+++ www/config_default.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'password',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
